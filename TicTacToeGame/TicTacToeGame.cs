6cb3d8da35071c51be3b7c180e1c8d55eaf5304d
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-02-03 10:15:27 +0530

    [Raja]Add creating char position for a players

---
+++ TicTacToeGame/TicTacToeGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacGame
+{
+    internal class TicTacToeGame
+    {
+        int playerOne;
+        int playerTwo;
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        public static void DispalyTicTacToeGame()
+        {
+            foreach(char c in arr)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
